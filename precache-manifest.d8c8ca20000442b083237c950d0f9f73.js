24c4eb8f6481d88632ff7f5a396e517ae708a580
gouth7 <gsubram5@ncsu.edu>
2019-06-10 12:25:57 -0400

    Updates

---
+++ precache-manifest.d8c8ca20000442b083237c950d0f9f73.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "44d571e7f68beca434cd5ae076bbb729",
+    "url": "/Travel/static/media/chicago2.44d571e7.jpg"
+  },
+  {
+    "revision": "8f9a500f66dd7c472ba1",
+    "url": "/Travel/static/js/main.8f9a500f.chunk.js"
+  },
+  {
+    "revision": "de40c803f9f82dc7c16d44a2200efab2",
+    "url": "/Travel/static/media/firefly2.de40c803.jpg"
+  },
+  {
+    "revision": "9ca68d6f89a601754d1a",
+    "url": "/Travel/static/js/runtime~main.9ca68d6f.js"
+  },
+  {
+    "revision": "32fb09409a3c02e4d7faeb4e5fcb5b3f",
+    "url": "/Travel/static/media/chicago1.32fb0940.jpg"
+  },
+  {
+    "revision": "0a67207448fe064f7ddc9fd9a7fc3ddc",
+    "url": "/Travel/static/media/firefly1.0a672074.jpg"
+  },
+  {
+    "revision": "f4b18fb79bcd5f1b5c448aa54c6eb89c",
+    "url": "/Travel/static/media/chicago3.f4b18fb7.jpg"
+  },
+  {
+    "revision": "585352034997b1332d234322d1ddfb10",
+    "url": "/Travel/static/media/chicago4.58535203.jpg"
+  },
+  {
+    "revision": "1a68e4b1115bf3d2274b",
+    "url": "/Travel/static/js/1.1a68e4b1.chunk.js"
+  },
+  {
+    "revision": "1a68e4b1115bf3d2274b",
+    "url": "/Travel/static/css/1.9905c167.chunk.css"
+  },
+  {
+    "revision": "634cdee2a4f33e8fb9add68f11519dd1",
+    "url": "/Travel/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/travel/precache-manifest.cc9a2403137f23f30e4dcd8a522cfdad.js"
+  "/Travel/precache-manifest.d8c8ca20000442b083237c950d0f9f73.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/travel/index.html", {
+workbox.routing.registerNavigationRoute("/Travel/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
